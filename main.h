4d564613f68e2267411da25b4eee88bbf637007a
CodeTalker314 <110099420+CodeTalker314@users.noreply.github.com>
2022-09-01 20:33:00 +0300

    Create main.h

---
+++ main.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "utils.h"
+#include "macros.h"
+#include "syntax.h"
+#include "ARRAY_FAKE_BASE_32.C"
+
+int main(int argc,char *argv[]);
