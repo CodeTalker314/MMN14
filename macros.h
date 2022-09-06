a685c118c782c808a21662059ad97f3189d728ef
CodeTalker314 <110099420+CodeTalker314@users.noreply.github.com>
2022-09-06 21:53:25 +0300

    Update macros.h

--- macros.h
+++ macros.h
@@ -16,4 +16,5 @@ void InsertName(struct Macros *temp, char line[]);
 void InsertContent(struct Macros *temp, FILE *fp);
 int IsMacroCall(char line[], FILE *fpw, struct Macros *tail);
 struct Macros* PreReadFile(char *filename, struct Macros *head);
-FILE * WritePreFile(char *filename, char *macrofile,struct Macros *tail);
+void WritePreFile(char *filename, char *macrofile,struct Macros *tail);
+void amRun( char* fullFileName , char* macroName , FILE* file );
